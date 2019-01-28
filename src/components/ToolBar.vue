723056d522eaca3d96c0ee4fbe698f47cc035331
techiegrace <g.adewumi@verifyme.ng>
2019-01-28 15:19:16 +0100

    Set up project again

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-toolbar app color="white">
+    <v-toolbar-title class="primary--text logo">
+      <router-link to="/">FashionBook</router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat class="flat-button">
+        <span>Hey there,</span>
+        <a class="ml-1">Sign in</a>
+      </v-btn>
+      <v-btn color="secondary" class="secondary-button" to="postad">Post New Ad</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<style lang="scss" scoped>
+.logo {
+  font-family: "Nanum Brush Script", cursive;
+  font-size: 40px;
+  font-weight: 600;
+  a {
+    text-decoration: unset;
+  }
+}
+.flat-button {
+  font-size: 23px;
+  text-transform: unset;
+  span {
+    font-family: "Nanum Brush Script", cursive;
+  }
+  a {
+    color: black;
+    font-size: 16px;
+    text-decoration: underline;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuetify from "vuetify";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#D30069",
+    secondary: "#035E6A",
+    btncolor: "#DAC416"
+  }
+});
 
 new Vue({
   router,
