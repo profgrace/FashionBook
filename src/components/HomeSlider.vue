f79a846a8a439e9b7a8dcfeebf0120aa5bd640bf
Grace <find.grace013@gmail.com>
2019-07-08 12:15:27 +0100

    Added extra demo slider for home page

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-carousel prev-icon="chevron_left"
-        next-icon="chevron_right"
-        hide-delimiters
-        light
-        class="slider">
+  <v-carousel
+    prev-icon="chevron_left"
+    next-icon="chevron_right"
+    hide-delimiters
+    light
+    class="slider"
+  >
     <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
   </v-carousel>
 </template>
@@ -14,6 +16,9 @@ export default {
       items: [
         {
           src: require("../assets/slider/slider1.png")
+        },
+        {
+          src: require("../assets/slider/slider3.jpg")
         }
       ]
     };
